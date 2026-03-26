88a1e11195909a1775fa9141be83f6322e2b3d61
hlibkovtun <glebblaid@gmail.com>
2026-03-26 21:56:45 +0100

    client_chat_part

--- client.py
+++ client.py
@@ -1,2 +1,31 @@
 import socket
 import threading
+
+nickname = input('Enter your Nickname: ')
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+def receive:
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print('Connection Error')
+            client.close()
+            break
+
+def write:
+    while True:
+        text = input("")
+        message = f"{nickname}: {text}"
+        client.send(message.encode('utf-8'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+write_thread = threading.Thread(target=write)
+write_thread.start()
